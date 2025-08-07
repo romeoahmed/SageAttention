f3507b4f53435fd5c8a0c1afa604af423a25c82b
woctordho <woctordho@outlook.com>
2025-08-08 01:40:47 +0800

    Simplify fake impl

--- sageattention/ops.py
+++ sageattention/ops.py
@@ -24,160 +24,107 @@ except Exception:
     _qattn_sm90 = None
     SM90_ENABLED = False
 
+
+def _return_lse(query, tensor_layout, return_lse):
+    batch_size = query.size(0)
+
+    if tensor_layout == 0:
+        num_qo_heads = query.size(2)
+        qo_len = query.size(1)
+    else:
+        num_qo_heads = query.size(1)
+        qo_len = query.size(2)
+
+    if return_lse:
+        lse = torch.empty((batch_size, num_qo_heads, qo_len), dtype=torch.float32, device=query.device)
+    else:
+        lse = torch.empty((0))
+    return lse
+
+
+def _fake_impl(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    output: torch.Tensor,
+    query_scale: torch.Tensor,
+    key_scale: torch.Tensor,
+    tensor_layout: int,
+    is_causal: int,
+    qk_quant_gran: int,
+    sm_scale: float,
+    return_lse: int,
+) -> torch.Tensor:
+    return _return_lse(query, tensor_layout, return_lse)
+
+
+def _fake_impl_v_mean(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    output: torch.Tensor,
+    query_scale: torch.Tensor,
+    key_scale: torch.Tensor,
+    value_mean: torch.Tensor,
+    tensor_layout: int,
+    is_causal: int,
+    qk_quant_gran: int,
+    sm_scale: float,
+    return_lse: int,
+) -> torch.Tensor:
+    return _return_lse(query, tensor_layout, return_lse)
+
+
+def _fake_impl_v_scale(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    output: torch.Tensor,
+    query_scale: torch.Tensor,
+    key_scale: torch.Tensor,
+    value_scale: torch.Tensor,
+    tensor_layout: int,
+    is_causal: int,
+    qk_quant_gran: int,
+    sm_scale: float,
+    return_lse: int,
+) -> torch.Tensor:
+    return _return_lse(query, tensor_layout, return_lse)
+
+
+def _fake_impl_v_scale_mean(
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    output: torch.Tensor,
+    query_scale: torch.Tensor,
+    key_scale: torch.Tensor,
+    value_scale: torch.Tensor,
+    value_mean: torch.Tensor,
+    tensor_layout: int,
+    is_causal: int,
+    qk_quant_gran: int,
+    sm_scale: float,
+    return_lse: int,
+) -> torch.Tensor:
+    return _return_lse(query, tensor_layout, return_lse)
+
+
 if SM80_ENABLED:
-    def sm80_qk_fake_impl(
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        output: torch.Tensor,
-        query_scale: torch.Tensor,
-        key_scale: torch.Tensor,
-        tensor_layout: int,
-        is_causal: int,
-        qk_quant_gran: int,
-        sm_scale: float,
-        return_lse: int,
-    ) -> torch.Tensor:
-        batch_size = query.size(0)
-
-        if tensor_layout == 0:
-            num_qo_heads = query.size(2)
-            qo_len = query.size(1)
-        else:
-            num_qo_heads = query.size(1)
-            qo_len = query.size(2)
-
-        if return_lse:
-            lse = torch.empty((batch_size, num_qo_heads, qo_len), dtype=torch.float32, device="cuda")
-        else:
-            lse = torch.empty((0))
-        return lse
-
-    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f32_attn")(sm80_qk_fake_impl)
-    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f16_attn")(sm80_qk_fake_impl)
-    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f16_attn_inst_buf")(sm80_qk_fake_impl)
-
-    @torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f16_fuse_v_mean_attn")
-    def qk_int8_sv_f16_accum_f16_fuse_v_mean_attn_fake_impl(
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        output: torch.Tensor,
-        query_scale: torch.Tensor,
-        key_scale: torch.Tensor,
-        value_mean: torch.Tensor,
-        tensor_layout: int,
-        is_causal: int,
-        qk_quant_gran: int,
-        sm_scale: float,
-        return_lse: int,
-    ) -> torch.Tensor:
-        return sm80_qk_fake_impl(
-            query, key, value, output, query_scale, key_scale, tensor_layout,
-            is_causal, qk_quant_gran, sm_scale, return_lse
-        )
+    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f32_attn")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f16_attn")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f16_attn_inst_buf")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm80::qk_int8_sv_f16_accum_f16_fuse_v_mean_attn")(_fake_impl_v_mean)
 
 if SM89_ENABLED:
-    def sm89_qk_fake_impl(
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        output: torch.Tensor,
-        query_scale: torch.Tensor,
-        key_scale: torch.Tensor,
-        value_scale: torch.Tensor,
-        tensor_layout: int,
-        is_causal: int,
-        qk_quant_gran: int,
-        sm_scale: float,
-        return_lse: int,
-    ) -> torch.Tensor:
-        batch_size = query.size(0)
-
-        if tensor_layout == 0:
-            num_qo_heads = query.size(2)
-            qo_len = query.size(1)
-        else:
-            num_qo_heads = query.size(1)
-            qo_len = query.size(2)
-
-        if return_lse:
-            lse = torch.empty((batch_size, num_qo_heads, qo_len), dtype=torch.float32, device="cuda")
-        else:
-            lse = torch.empty((0))
-        return lse
-
-    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn")(sm89_qk_fake_impl)
-    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf")(sm89_qk_fake_impl)
-    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f16_fuse_v_scale_attn_inst_buf")(sm89_qk_fake_impl)
-
-    @torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_fuse_v_scale_fuse_v_mean_attn")
-    def qk_int8_sv_f8_accum_f32_fuse_v_scale_fuse_v_mean_attn_fake_impl(
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        output: torch.Tensor,
-        query_scale: torch.Tensor,
-        key_scale: torch.Tensor,
-        value_scale: torch.Tensor,
-        value_mean: torch.Tensor,
-        tensor_layout: int,
-        is_causal: int,
-        qk_quant_gran: int,
-        sm_scale: float,
-        return_lse: int,
-    ) -> torch.Tensor:
-        return sm89_qk_fake_impl(
-            query, key, value, output, query_scale, key_scale, value_scale,
-            tensor_layout, is_causal, qk_quant_gran, sm_scale, return_lse
-        )
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_attn")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn")(_fake_impl_v_scale)
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_fuse_v_scale_fuse_v_mean_attn")(_fake_impl_v_scale_mean)
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_attn_inst_buf")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f16_attn_inst_buf")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf")(_fake_impl_v_scale)
+    torch.library.register_fake("sageattention_qattn_sm89::qk_int8_sv_f8_accum_f16_fuse_v_scale_attn_inst_buf")(_fake_impl_v_scale)
 
 if SM90_ENABLED:
-    @torch.library.register_fake("sageattention_qattn_sm90::qk_int8_sv_f8_accum_f32_attn_inst_buf")
-    def qk_int8_sv_f8_accum_f32_attn_inst_buf_fake_impl(
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        output: torch.Tensor,
-        query_scale: torch.Tensor,
-        key_scale: torch.Tensor,
-        tensor_layout: int,
-        is_causal: int,
-        qk_quant_gran: int,
-        sm_scale: float,
-        return_lse: int,
-    ) -> torch.Tensor:
-        batch_size = query.size(0)
-
-        if tensor_layout == 0:
-            num_qo_heads = query.size(2)
-            qo_len = query.size(1)
-        else:
-            num_qo_heads = query.size(1)
-            qo_len = query.size(2)
-
-        if return_lse:
-            lse = torch.empty((batch_size, num_qo_heads, qo_len), dtype=torch.float32, device="cuda")
-        else:
-            lse = torch.empty((0))
-        return lse
-
-    @torch.library.register_fake("sageattention_qattn_sm90::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf")
-    def qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf_fake_impl(
-        query: torch.Tensor,
-        key: torch.Tensor,
-        value: torch.Tensor,
-        output: torch.Tensor,
-        query_scale: torch.Tensor,
-        key_scale: torch.Tensor,
-        value_scale: torch.Tensor,
-        tensor_layout: int,
-        is_causal: int,
-        qk_quant_gran: int,
-        sm_scale: float,
-        return_lse: int,
-    ) -> torch.Tensor:
-        return qk_int8_sv_f8_accum_f32_attn_inst_buf_fake_impl(
-            query, key, value, output, query_scale, key_scale, tensor_layout,
-            is_causal, qk_quant_gran, sm_scale, return_lse
-        )
+    torch.library.register_fake("sageattention_qattn_sm90::qk_int8_sv_f8_accum_f32_attn_inst_buf")(_fake_impl)
+    torch.library.register_fake("sageattention_qattn_sm90::qk_int8_sv_f8_accum_f32_fuse_v_scale_attn_inst_buf")(_fake_impl_v_scale)
